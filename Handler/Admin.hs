ddba87cde3a307ca4e7e288924e0733f3480cdb8
Patrick Hurst <phurst@amateurtopologist.com>
2012-04-01 06:31:28 -0400

    implement tag-field logic (but not associating posts with tags)

--- Handler/Admin.hs
+++ Handler/Admin.hs
@@ -1,11 +1,19 @@
 module Handler.Admin where
 
 import Import
+import qualified Data.Text as T
 
-postForm :: Form Post
-postForm = renderDivs $ Post
+data PostF = PostF {
+    title :: Text
+  , body :: Text
+  , tags :: [Text]
+  } deriving Show
+
+postForm :: Form PostF
+postForm = renderDivs $ PostF
            <$> areq textField "Title" Nothing
            <*> unTextarea `fmap` areq textareaField "Body" Nothing
+           <*> areq tagListField "Tags" Nothing
 
 
 getAdminR :: Handler RepHtml
@@ -21,6 +29,15 @@ postAdminR :: Handler RepHtml
 postAdminR = do
   ((result, widget), enctype) <- runFormPost postForm
   case result of
-    FormSuccess post -> do
-      runDB $ insert post
+    FormSuccess postf -> do
+      runDB $ mapM (insert . Tag) (tags postf) >> insert (Post (title postf) (body postf))
+
       redirect RootR
+
+-- A field for a list of tags, which are comma-separated.
+tagListField :: Field sub master [Text]
+tagListField = Field {
+    fieldParse = \vals -> return . Right . Just . map T.strip $ vals >>= T.splitOn ","
+  , fieldView = \idAttr nameAttr theClass result _ -> [whamlet|
+<input id=#{idAttr} name=#{nameAttr} :not (null theClass):class="#{T.intercalate " " theClass}" type=text value=#{either (const "") (T.intercalate ", ") result}>
+|] }
