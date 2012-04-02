3b6257cfda806a30b52e64a6555c562b868683d9
Patrick Hurst <phurst@amateurtopologist.com>
2012-04-02 03:47:47 -0400

    show tags on post widget

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -1,7 +1,10 @@
 module Handler.Root where
 
+import qualified Data.Text as T
+import Database.Persist.Query.Join
 import Import
 import Text.Blaze
+
 -- This is a handler function for the GET request method on the RootR
 -- resource pattern. All of your resource patterns are defined in
 -- config/routes
@@ -11,7 +14,28 @@ import Text.Blaze
 -- inclined, or create a single monolithic file.
 getRootR :: Handler RepHtml
 getRootR = do
-  posts <- map entityVal `fmap` (runDB $ selectList [] [Desc PostId])
+  posts' <- runDB $ selectList [] [Desc PostId]
+  let posts = map postWidget posts'
   defaultLayout $ do
     setTitle "Home"
     $(widgetFile "homepage")
+
+-- | Map a Post entity to the list of Tag entities that it has as tags.
+tagsFor :: (backend ~ YesodPersistBackend Milagos) =>
+           Entity (PostGeneric backend) -> YesodDB sub Milagos [Entity (TagGeneric backend)]
+tagsFor post = do
+  som <- runJoin $ (selectOneMany (PostTagTagId <-.) postTagTagId)
+           { somFilterMany = [PostTagPostId ==. entityKey post] }
+  return $ map fst som
+
+-- | Turn a Post entity into a widget.
+postWidget :: Entity (PostGeneric (YesodPersistBackend Milagos)) -> Widget
+postWidget postEnt = do
+  let post = entityVal postEnt
+  tags <- map (tagName . entityVal) <$> (lift . runDB $ tagsFor postEnt)
+  liftIO $ print (length tags)
+  [whamlet|
+<h1>#{postTitle post}
+<div>#{preEscapedText $ postBody post}
+$if not (null tags)
+  <div>Tagged: #{T.intercalate ", " tags}|]
