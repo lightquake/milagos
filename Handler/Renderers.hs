fc20aff058c5d1ebe4b4a2d6262ab03098d9a08b
Patrick Hurst <phurst@amateurtopologist.com>
2012-04-09 03:10:45 -0400

    style older/newer links

--- Handler/Renderers.hs
+++ Handler/Renderers.hs
@@ -35,13 +35,7 @@ pageWidget page perPage total =
   -- total number of pages is total / per + 1 (or + 0 if it's an even
   -- division)
   let pages = (\(n, r) -> n + min r 1) $ total `divMod` perPage in
-    when (pages > 1) $ do
-      [whamlet|
-<div .navigation>
-  <div .prev>^{prevPage}
-  $if page < pages
-    <div .next>^{nextPage}
-|]
+    when (pages > 1) $(widgetFile "navigation")
 
 -- | A widget that links to the previous page. Is invisible if the
 -- current page is not at least 1.
