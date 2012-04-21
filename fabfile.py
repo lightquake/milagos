4f93fe5a378742c5787b8a48d0116e46fbd45738
Patrick Hurst <phurst@amateurtopologist.com>
2012-04-21 15:46:16 -0400

    deploy config via fabric

--- fabfile.py
+++ fabfile.py
@@ -10,4 +10,5 @@ def deploy():
     local("scp cabal-dev/bin/milagos phurst@amateurtopologist.com:/srv/milagos/")
     local("rm -rf static/tmp/*")
     local("rsync --progress -r static phurst@amateurtopologist.com:/srv/milagos/static")
+    local("rsync --progress -r config phurst@amateurtopologist.com:/srv/milagos/config")
     run("supervisorctl start webdaemons:milagos")
